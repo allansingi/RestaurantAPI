9a3ffcbf4269db7f3653e36cf339f388b16c80aa
Allan Borges <allan-singi.borges@capgemini.com>
2025-09-16 14:46:10 +0100

    sonar refactors

--- src/test/java/pt/allanborges/restaurant/RestaurantApplicationTests.java
+++ src/test/java/pt/allanborges/restaurant/RestaurantApplicationTests.java
@@ -8,6 +8,7 @@ class RestaurantApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Add a nested comment explaining why this method is empty, throw an UnsupportedOperationException or complete the implementation.
 	}
 
 }
